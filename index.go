92fe5f45938969b0dfb31b35949499d374eb2b62
Narongsak <narongsak.chobsri@gmail.com>
2018-03-01 22:02:20 +0700

    Initial project

---
+++ index.go
@@ -0,0 +1,12 @@
+package main
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "My first golang")
+	})
+	http.ListenAndServe(":8080", nil)
+}
