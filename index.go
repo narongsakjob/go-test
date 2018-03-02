488b3056800f1818b0ce5e5005e6b874c75e18e1
Narongsak <narongsak.chobsri@gmail.com>
2018-03-02 23:01:11 +0700

    Add test new path

--- index.go
+++ index.go
@@ -5,8 +5,19 @@ import (
 )
 
 func main() {
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "My first golang")
 	})
+	http.HandleFunc("/user", user)
+	http.HandleFunc("/product", product)
 	http.ListenAndServe(":8080", nil)
+}
+
+func product(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Product Request")
+}
+
+func user(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "User Request")
 }
